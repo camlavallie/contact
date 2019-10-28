5bcb973cf66c05c15c924c873cbbf3a20c5082ed
Cameron LaVallie <camlavallie@gmail.com>
2019-10-28 16:24:50 -0500

    make it better

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Header() {
   return (
     <nav className="navbar navbar-dark bg-dark">
-  <a className="navbar-brand" href="/"><h2>Frontend React Challenge</h2></a>
+  <a className="navbar-brand" href="/">
+    <h3 style={{padding:'10px', margin:'10px'}}>
+      Frontend React Challenge
+    </h3>
+  </a>
 
 </nav>
   )
