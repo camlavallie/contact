cd4871ab87e07a017581429c959d89154cfcd08a
Cameron LaVallie <camlavallie@gmail.com>
2019-10-28 23:05:48 -0500

    css fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,25 +5,28 @@ export default function Footer() {
       <nav 
         style={{marginTop:'30px'}}
         className="navbar navbar bg-light">
+
           <a 
             className="navbar-brand" 
             href="https://github.com/camlavallie/contact"
             target="blank"
             >
-
             <h3 
             style={{
               padding:'10px', 
               margin:'10px'
               }}>
               Source Code!
-            </h3>            
+            </h3>          
            </a>
+
+
            <h4 style={{ 
              color:'lightgrey', 
              padding:'10px', 
              margin:'10px'}}> 
              Copyright &copy; {new Date().getFullYear()} CAM</h4>
+             
         </nav>
   )
 }
